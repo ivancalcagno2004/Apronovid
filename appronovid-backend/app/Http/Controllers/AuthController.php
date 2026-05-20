3dfd14395ad9eb5a530b61c87de30f0fa6c3bb26
Iván Calcagno <icalcagno@alumnos.exa.unicen.edu.ar>
2026-05-20 17:12:59 -0300

    feat: Update user role validation in registration to include 'voluntario'

--- appronovid-backend/app/Http/Controllers/AuthController.php
+++ appronovid-backend/app/Http/Controllers/AuthController.php
@@ -14,7 +14,7 @@ class AuthController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:8',
-            'role' => 'required|in:oyente,narrador',
+            'role' => 'required|in:oyente,voluntario',
         ]);
 
         $user = User::create([
